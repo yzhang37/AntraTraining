cdbb388153f24cced4cf3d4e66b6ae13948b318c
青椒 <johnyin@bu.edu>
2023-02-16 00:19:38 -0500

    Day 2. 7-9

--- SQL/Day2/query.sql
+++ SQL/Day2/query.sql
@@ -44,14 +44,25 @@ HAVING SUM(Quantity) < 100;
 --    | Shelf | ProductID | TheSum |
 --    | ----- | --------- | ------ |
 --    |       |           |        |
-
+SELECT ProductID, Shelf, SUM(Quantity) AS TheSum
+FROM Production.ProductInventory
+WHERE LocationID = 40
+GROUP BY ProductID, Shelf
+HAVING SUM(Quantity) < 100;
 
 -- 8. Write the query to list the average quantity for products where column LocationID has the value of 10 from the table Production.ProductInventory table.
+SELECT AVG(Quantity) AS AvgOfQuantity
+FROM Production.ProductInventory
+WHERE LocationID = 10;
+
 -- 9. Write query to see the average quantity of products by shelf from the table Production.ProductInventory
 
 --    | ProductID | Shelf | TheAvg |
 --    | --------- | ----- | ------ |
 --    |           |       |        |
+SELECT ProductID, Shelf, AVG(Quantity) AS TheAvg
+FROM Production.ProductInventory
+GROUP BY ProductID, Shelf;
 
 -- 10. Write query to see the average quantity of products by shelf excluding rows that has the value of N/A in the column Shelf from the table Production.ProductInventory
 
@@ -59,11 +70,13 @@ HAVING SUM(Quantity) < 100;
 --     | --------- | ----- | ------ |
 --     |           |       |        |
 
+
 -- 11. List the members (rows) and average list price in the Production.Product table. This should be grouped independently over the Color and the Class column. Exclude the rows where Color or Class are null.
 
 --     | Color | Class | TheCount | AvgPrice |
 --     | ----- | ----- | -------- | -------- |
 --     |       |       |          |          |
+
 
 -- ## Joins:
 
