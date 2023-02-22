32254cdafa104ecb2fab663cc089123f5a452819
青椒 <johnyin@bu.edu>
2023-02-21 19:58:02 -0500

    Complete Day5. Assign 1

--- C#/Day. 5/Chapter03/ConsoleApp1/Program1.cs
+++ C#/Day. 5/Chapter03/ConsoleApp1/Program1.cs
@@ -1,4 +1,5 @@
-﻿// FizzBuzz
+﻿using System.Text;
+// FizzBuzz
 
 void FuzzBuzz() {
     for (int i = 1; i <= 100; ++i)
@@ -14,17 +15,17 @@ void FuzzBuzz() {
     }
 }
 
-FuzzBuzz();
+// FuzzBuzz();
 
 void GuessNumber()
 {
     int correctNumber = new Random().Next(3) + 1;
-    int guess = 0;
+    int guess;
     do
     {
         try
         {
-            guess = int.Parse(Console.ReadLine());
+            guess = int.Parse(Console.ReadLine()??"0");
             if (guess < 1 || guess > 3)
                 Console.WriteLine("Guess should within 1 to 3");
             else if (guess < correctNumber)
@@ -44,4 +45,83 @@ void GuessNumber()
     } while (true);
 }
 
-GuessNumber();
+// GuessNumber();
+
+void PrintPyramid(int height)
+{
+    int width = height * 2 - 1;
+    var sb = new StringBuilder();
+    sb.Append(' ', width);
+    for (int i = 0; i < height; ++i)
+    {
+        sb[width / 2 + i] = '*';
+        sb[width / 2 - i] = '*';
+        Console.WriteLine(sb);
+    }
+}
+
+// int height = Int32.Parse(Console.ReadLine()??"1");
+// PrintPyramid(height);
+
+void GetAge()
+{
+    Console.WriteLine("Enter your birth date:");
+    int birthYear = Int32.Parse(Console.ReadLine()??"0");
+    int birthMonth = Int32.Parse(Console.ReadLine()??"0");
+    int birthDay = Int32.Parse(Console.ReadLine()??"0");
+    if (birthMonth > 12 || birthMonth < 1)
+    {
+        Console.WriteLine("Invalid month");
+        return;
+    }
+    if (birthDay > 31 || birthDay < 1)
+    {
+        Console.WriteLine("Invalid day");
+        return;
+    }
+
+    int age = DateTime.Now.Year - birthYear - 1;
+    if (DateTime.Now.Month > birthMonth)
+        age++;
+    else if (DateTime.Now.Month == birthMonth && DateTime.Now.Day >= birthDay)
+        age++;
+    Console.WriteLine($"Your age is { age }");
+    var birthDate = new DateTime(birthYear, birthMonth, birthDay);
+    // Print 10000 days after your birth
+    var newDate = birthDate.AddDays(10000);
+    Console.WriteLine(@$"Next 27 years anniversary (10000 days) is:
+{newDate.Day}/{newDate.Month}/{newDate.Year}");
+}
+
+// GetAge();
+
+void TimeGreeting()
+{
+    DateTime now = DateTime.Now;
+    if (now.Hour is >= 6 and < 12)
+        Console.WriteLine("Good morning");
+    else if (now.Hour is >= 12 and < 18)
+        Console.WriteLine("Good afternoon");
+    else if (now.Hour is >= 18 and < 22)
+        Console.WriteLine("Good evening");
+    else
+        Console.WriteLine("Good night");
+}
+
+// TimeGreeting();
+
+void IncrementByNestedFor()
+{
+    for (int incr = 1; incr <= 4; ++incr)
+    {
+        Console.Write('0');
+        for (int i = incr; i <= 24; i+=incr)
+        {
+            Console.Write(',');
+            Console.Write(i);
+        }
+        Console.Write('\n');
+    }
+}
+
+// IncrementByNestedFor();
